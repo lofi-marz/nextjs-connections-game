816c1e072b6e6d7ac94a3dc1d1898dd0c96e3d74
Omari <marzipant.e@gmail.com>
2023-12-15 10:09:50 +0000

    Add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,14 @@ import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Analytics } from '@vercel/analytics/react';
 const queryClient = new QueryClient();
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <QueryClientProvider client={queryClient}>
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="dark"
-                enableSystem
-                >
+            <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
                 <Component {...pageProps} />
+                <Analytics />
             </ThemeProvider>
         </QueryClientProvider>
     );
